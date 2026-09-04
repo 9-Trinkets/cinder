c74724f2125eb1686c88ec2c29ae9a2e55d4de1d
Li-Hsuan Lung <lihsuanlung@gmail.com>
2026-09-04 01:08:25 -0400

    refactor: wrap all routes in a shared ErrorBoundary

--- cinder-web-ui/src/App.tsx
+++ cinder-web-ui/src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider, useAuth } from './auth'
 import { ToastProvider } from './components/Toast'
+import ErrorBoundary from './components/ErrorBoundary'
 import LoginPage from './pages/LoginPage'
 import GamesPage from './pages/GamesPage'
 import PackDetailPage from './pages/PackDetailPage'
@@ -22,6 +23,7 @@ export default function App() {
   return (
     <AuthProvider>
       <ToastProvider>
+        <ErrorBoundary>
         <Routes>
           <Route path="/login" element={<RedirectIfLoggedIn />} />
           <Route path="/games" element={<ProtectedRoute><GamesPage /></ProtectedRoute>} />
@@ -29,6 +31,7 @@ export default function App() {
           <Route path="/games/:id" element={<ProtectedRoute><GamePage /></ProtectedRoute>} />
           <Route path="*" element={<Navigate to="/login" replace />} />
         </Routes>
+        </ErrorBoundary>
       </ToastProvider>
     </AuthProvider>
   )
